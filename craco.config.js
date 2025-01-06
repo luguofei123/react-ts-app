74b15c013bd4d7f0ddb6ae37002b4ddce8610ced
luguofei <lugfa@yonyou.com>
2025-01-06 16:20:32 +0800

    打包配置

--- craco.config.js
+++ craco.config.js
@@ -4,7 +4,7 @@
  * @Author: lugfa
  * @Date: 2024-08-29 19:12:54
  * @LastEditors: lugfa
- * @LastEditTime: 2024-08-30 09:44:05
+ * @LastEditTime: 2025-01-06 16:18:59
  * @FilePath: /react-ts-app/craco.config.js
  */
 const path = require('path')
@@ -22,6 +22,12 @@ module.exports = {
     /* 别名 */
     alias: {
       '@': resolve('src')
+    },
+    // 更改build打包文件名称为dist
+    configure: (webpackConfig, { env, paths }) => {
+      webpackConfig.output.path = path.resolve(__dirname, 'dist')
+      paths.appBuild = path.resolve(__dirname, 'dist')
+      return webpackConfig
     }
   },
   devServer: {
